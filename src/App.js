37c04e63c64e7bdf1090e362104d4741434e9ed7
naharki <sujannaharki68@gmail.com>
2023-06-15 11:37:50 +0545

    Some changes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Card from './components/Card';
 import './App.css';
+import Testomonial from './components/Testomonial';
 
 function App() {
   return (
     <div className="App">
+      <Testomonial />
       <Card />
     </div>
   );
---
+++ src/components/Testomonial.jsx
@@ -0,0 +1,10 @@
+import "../styles/Testomonial.css";
+
+function Testomonial() {
+  return (
+    <>
+      <h1 className="title">Our Member</h1>
+    </>
+  );
+}
+export default Testomonial;
